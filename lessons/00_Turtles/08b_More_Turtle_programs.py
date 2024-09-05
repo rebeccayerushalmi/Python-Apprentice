5f99706cf6055d9cafdb9e6064a3f7bf14965175
rebeccayerushalmi <rebeccashilohyer@gmail.com>
2024-09-05 20:42:41 +0000

    donee

--- lessons/00_Turtles/08b_More_Turtle_programs.py
+++ lessons/00_Turtles/08b_More_Turtle_programs.py
@@ -8,3 +8,5 @@ Then change the code so that the turtle has a different image ( look in the 'ima
 directory ) and moves to the corners of the screen in a square pattern. 
 """
 
+# Change the Turtle Image
+set_turtle
